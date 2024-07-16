0c09fdde9a49817fa1c17c2ba648d434d9e90c5d
Marco Antônio Dal Vesco <marco.dalvesco@gmail.com>
2024-07-16 12:08:18 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import RecipeCard from './components/RecipeCard/RecipeCard.jsx'
+import GlobalStyle from '../assets/styles/GlobalStyles'
+
 
 function App() {
 
 
   return (
     <div>
+      <GlobalStyle />
       <Route path="/recipe-page" element={<RecipeCard />} />
       <Route path="/recipe/:id" element={<RecipeCard />} />
     </div>
